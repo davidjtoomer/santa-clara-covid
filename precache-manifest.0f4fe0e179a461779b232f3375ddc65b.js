9b59cad6aeb39678b1afe469189cd3e8ba5f4b75
David Toomer <djtoomer22@gmail.com>
2020-08-05 04:08:29 -0400

    Updates

---
+++ precache-manifest.0f4fe0e179a461779b232f3375ddc65b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2007b7393037b17592c1dc2cfde4ec2a",
+    "url": "/santa-clara-covid/index.html"
+  },
+  {
+    "revision": "67dba961ea3086c753ac",
+    "url": "/santa-clara-covid/static/css/main.5bd63142.chunk.css"
+  },
+  {
+    "revision": "e33f6ec2712e2a287c97",
+    "url": "/santa-clara-covid/static/js/2.86ac6c28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/santa-clara-covid/static/js/2.86ac6c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67dba961ea3086c753ac",
+    "url": "/santa-clara-covid/static/js/main.620dd999.chunk.js"
+  },
+  {
+    "revision": "81339f9d19c458ea2581",
+    "url": "/santa-clara-covid/static/js/runtime-main.9f2c59df.js"
+  }
+]);
