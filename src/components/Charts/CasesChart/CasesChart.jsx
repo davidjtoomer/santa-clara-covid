a229f725ea11df3fe6c9e99bcec3a718f76a1bfb
David Toomer <djtoomer22@gmail.com>
2020-08-06 07:52:35 -0400

    add temporal graphs through chart.js

---
+++ src/components/Charts/CasesChart/CasesChart.jsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+import {Line} from 'react-chartjs-2';
+import 'chartjs-plugin-zoom';
+
+const CasesChart = () => {
+  const case_data = useSelector(state => state.data.cases);
+
+  return (
+    <div className='cases-chart-container'>
+      <div className='cases-chart-title'>
+        <h5>Confirmed Cases in Santa Clara County</h5>
+      </div>
+      <div className='cases-chart'>
+        {case_data &&
+          <Line
+            data = {{
+              labels: case_data.labels,
+              datasets: [
+                {
+                  label: 'New Confirmed Cases',
+                  data: case_data.new_count_confirmed,
+                  yAxisID: 'new',
+                  borderColor: '#82c9e6',
+                  backgroundColor: 'transparent',
+                  pointBackgroundColor: 'transparent',
+                  pointBorderColor: 'transparent',
+                  pointRadius: 4
+                }, 
+                {
+                  label: 'Cumulative Confirmed Cases',
+                  data: case_data.total_count_confirmed,
+                  yAxisID: 'cumulative',
+                  borderColor: '#f45060',
+                  backgroundColor: 'transparent',
+                  pointBackgroundColor: 'transparent',
+                  pointBorderColor: 'transparent',
+                  pointRadius: 4
+                }
+              ]
+            }}
+            options = {{
+              responsive: true,
+              pan: {
+                enabled: true,
+                mode: 'xy'
+              },
+              zoom: {
+                enabled: true,
+                mode: 'xy'
+              },
+              scales: {
+                xAxes: [{
+                  type: 'time',
+                  time: {
+                    unit: 'day'
+                  },
+                  gridLines: {
+                    color: '#1e2130'
+                  }
+                }],
+                yAxes: [{
+                  id: 'new',
+                  type: 'linear',
+                  position: 'left',
+                  gridLines: {
+                    color: '#1e2130'
+                  }
+                }, {
+                  id: 'cumulative',
+                  type: 'linear',
+                  position: 'right',
+                  gridLines: {
+                    color: '#1e2130'
+                  }
+                }]
+              }
+            }}
+          />
+        }
+      </div>
+    </div>
+  );
+}
+
+export default CasesChart;
---
+++ src/components/Charts/Charts.jsx
@@ -0,0 +1,40 @@
+import React, {useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {fetchAllData} from '../../store/actions';
+import CasesChart from './CasesChart/CasesChart';
+import DeathsChart from './DeathsChart/DeathsChart';
+import {BeatLoader} from 'react-spinners';
+import './Charts.scss'
+
+const Charts = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.base.loading);
+
+  useEffect(() => {
+    dispatch(fetchAllData());
+  }, []);
+
+  return (
+    <React.Fragment>
+      {!loading
+        ? <div className='charts'>
+            <div className='time-charts'>
+              <CasesChart/>
+              <DeathsChart/>
+            </div>
+            <div className='demographic-charts'>
+            </div>
+          </div>
+        : <div className='beat-loader'>
+            <BeatLoader
+              color='#82c9e6'
+              size='25'
+              margin='15'
+            />
+          </div>
+      }
+    </React.Fragment>
+  );
+};
+
+export default Charts;
